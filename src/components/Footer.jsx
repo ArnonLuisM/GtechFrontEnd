5c57a996d85e48d2bf66d49769d8f27c06c7d4bc
Lorrany <lorrany.acopiara@gmail.com>
2026-02-01 22:06:55 -0300

    fazendo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import {NavLink} from  "react-router-dom"
+import styled from "styled-components";
+import Logo from './Logo';
+import facebook from "../assets/facebook.png";
+import instagram from "../assets/instagram.png";
+import twitter from "../assets/twitter.png";
+const FooterContainer = styled.footer`
+
+  background-color: var(--dark-gray);
+  font-family: inter;
+  padding-inline: 5%;
+  letter-spacing: 0.75px;
+
+
+`
 
 const Footer = () => {
   return (
-    <>
-        <h1>Footer</h1>
-    </>
+    <FooterContainer>
+        <div>
+          <Logo/>
+          <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+          <a href="#"><img src={facebook} alt="" /></a>
+          <a href="#"><img src={instagram} alt="" /></a>
+          <a href="#"><img src={twitter} alt="" /></a>
+        </div>
+        <div>
+          <hr/>
+          <h6>@ 2026 Digital Store</h6>
+        </div>
+    </FooterContainer>
   )
 }
 
