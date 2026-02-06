e940eb76dc3de80302c5959964470a1e5794e179
Lorrany <lorrany.acopiara@gmail.com>
2026-02-06 05:24:13 -0300

    atualização do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {NavLink} from  "react-router-dom"
 import styled from "styled-components";
-import Logo from './Logo';
+import Logob from '../assets/logoBranca.png';
 import facebook from "../assets/facebook.png";
 import instagram from "../assets/instagram.png";
 import twitter from "../assets/twitter.png";
@@ -10,27 +10,71 @@ const FooterContainer = styled.footer`
   background-color: var(--dark-gray);
   font-family: inter;
   padding-inline: 5%;
+  padding-top: 5%;
   letter-spacing: 0.75px;
-
-
+  color: var(--white);
+  justify-content: flex-start;
+  & .principal{
+    display: flex;
+    justify-content: space-between;
+  }
+  & .resumo h3{
+    display: flex;
+    padding: 2% 10% 2% 2%;
+    padding: 5%;
+  }
+  & .redes {
+    display: flex;
+    gap: 5%;
+  }
+  & .informação{
+    padding: 2% 2% 2% 2%;
+  } 
+  & .categorias{
+    padding: 2% 2% 2% 2%;
+  }
+  & .contato{
+    padding: 2% 2% 2% 2%;
+  }
+  & .ultimo {
+    display: flex;
+    justify-content: center;
+    padding: 2% 2% 2% 2%;
+  }
+  
 `
 
 const Footer = () => {
   return (
     <FooterContainer>
-        <div>
-          <Logo/>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
-          <a href="#"><img src={facebook} alt="" /></a>
-          <a href="#"><img src={instagram} alt="" /></a>
-          <a href="#"><img src={twitter} alt="" /></a>
+        <div className="principal">
+          <div className="resumo">
+            <figure>
+              <img src={Logob} alt="Logo da loja" />
+            </figure>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+            <div className="redes">
+              <a href="#"><img src={facebook} alt="Facebook da loja" /></a>
+              <a href="#"><img src={instagram} alt="Instagram da loja" /></a>
+              <a href="#"><img src={twitter} alt="Twitter da loja" /></a>
+            </div>
+          </div>
+          <div className="informação">
+            
+          </div>
+          <div className="categorias">
+            
+          </div>
+          <div className="contato">
+            
+          </div>
         </div>
-        <div>
-          <hr/>
-          <h6>@ 2026 Digital Store</h6>
+        <hr/>
+        <div className="ultimo">
+          <h5>@ 2026 Digital Store</h5>
         </div>
     </FooterContainer>
   )
 }
 
-export default Footer
+export default Footer;
