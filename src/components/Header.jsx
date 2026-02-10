dc4db1b1099d6cbac58b9e3798eac19b6dfd84b6
icaro <icarolamartine06@hotmail.com>
2026-02-09 21:06:55 -0300

    finalizada a parte grafica do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import {NavLink} from  "react-router-dom"
+import {NavLink, Route, Routes} from  "react-router-dom"
 import styled from "styled-components";
 import Logo from './Logo';
 import Search from "../assets/Search.png"
 import Buy from "../assets/Buy.png"
+import AppRoutes from '../router/AppRoutes';
 const HeaderContainer = styled.header`
     
     font-family: inter;
@@ -47,22 +48,42 @@ const HeaderContainer = styled.header`
             color: var(--light-gray-3);
             border-radius: 8px;
         }
-        & input{
-            color: var(--dark-gray-2);
-            width: 40vw;
-            height: 2vw;
-            padding: 2%;
-            border: none;
-            border-radius: 8px;
-            background-color: var(--light-gray-3);
-            background-image: image(Search);
-            letter-spacing: 0.75px;
-        }
+        
 
     }
+`
+const SearchContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
     
-
-
+    width: 40vw;
+    height: 2vw;
+    border: none;
+    border-radius: 8px;
+    background-color: var(--light-gray-3);
+    letter-spacing: 0.75px;
+    align-items: center;
+    padding-inline: 2%;
+    padding-block: 1%;
+        & input{
+            font-family: inter;
+            border: none;
+            background-color: var(--light-gray-3);
+            letter-spacing: 0.75px;
+            font-weight: bold;
+            height: 3vw;
+            width: 38vw;
+            &:focus{
+                outline: none;
+                box-shadow:none;
+            }
+            & input::placeholder{
+                color: var(--light-gray-3);
+                font-weight: normal;
+                opacity: 0.5;
+            }
+            
+        }
 `
 const Header = () => {
   return (
@@ -70,8 +91,11 @@ const Header = () => {
         
         <div>
             <Logo/>
-            <input type="search" name="Pesquisar" id="" placeholder='Pesquisar produto...'/>
-            <span><img src={Search} alt="Pesquisa" /></span>
+            <SearchContainer>
+                <input type="search" name="Pesquisar" id="pesquisa" placeholder='Pesquisar produto...'/>
+                <span><img src={Search} alt="Pesquisa" /></span>
+            </SearchContainer>
+            
             <a href="#" className='cadastro'>Cadastrar-se</a>
             <a href='#' className='entrar'>Entrar</a>
             <a href="#" ><img src={Buy} alt="Carrinho de compras" /></a>
