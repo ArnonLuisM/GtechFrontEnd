4c138cd533e32a42593b4b50c970bf2c8c8fc438
Lorrany <lorrany.acopiara@gmail.com>
2026-02-18 21:42:29 -0300

    footer finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,33 +16,161 @@ const FooterContainer = styled.footer`
   justify-content: flex-start;
   & .principal{
     display: flex;
-    justify-content: space-between;
+  }
+  & .resumo {
+    width: 30%;
   }
   & .resumo h3{
     display: flex;
-    padding: 2% 10% 2% 2%;
-    padding: 5%;
+    padding: 2% 2% 2% 2%;
+    margin-bottom: 5%;
+    margin-top: 5%;
+    font-weight: 400;
   }
   & .redes {
     display: flex;
     gap: 5%;
   }
+  & .tudo{
+    display: flex;
+    justify-content: space-around;
+    width: 80%;
+  }
   & .informação{
     padding: 2% 2% 2% 2%;
   } 
   & .categorias{
-    padding: 2% 2% 2% 2%;
+    padding: 2% 10% 2% 2%;
   }
   & .contato{
     padding: 2% 2% 2% 2%;
+    width: 20%;
+  }
+  & .listass {
+    list-style: none;
+    margin-bottom: 15%;
+    font-weight: 400;
+  }
+  & .listas1 {
+    list-style: none;
+    margin-bottom: 5%;
+    font-weight: 400;
+  }
+  & .links{ 
+    text-decoration: none;
+    color: var(--white);
+  }
+  & .links:hover { 
+    color: var(--primary);
+  }
+  & .titulo{
+    margin-bottom: 30%;
   }
   & .ultimo {
     display: flex;
     justify-content: center;
     padding: 2% 2% 2% 2%;
   }
-  
-`
+  `
+const informacao = [
+  {
+    text: "Sobre Drip Store",
+    link: "#"
+  },
+  {
+    text: "Segurança",
+    link: "#"
+  },
+  {
+    text: "Wishlist",
+    link: "#"
+  },
+  {
+    text: "Blog",
+    link: "#"
+  },
+  {
+    text: "Trabalhe Conosco",
+    link: "#"
+  },
+  {
+    text: "Meus Pedidos",
+    link: "#"
+  }
+]
+
+const categorias = [
+  {
+    text: "Camisetas",
+    link: "#"
+  },
+  {
+    text: "Calças",
+    link: "#"
+  },
+  {
+    text: "Bonés",
+    link: "#"
+  },
+  {
+    text: "Headphones",
+    link: "#"
+  },
+  {
+    text: "Tênis",
+    link: "#"
+  }
+]
+
+const contato = [
+  {
+    text: "Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161",
+    num: "(85) 3051-3411"
+  }
+]
+
+function ListaInformacoes() {
+  return (
+    <ul>
+      {informacao.map((item, index) => (
+        <li key={index} className="listass">
+          <NavLink to={item.link} className="links">
+            {item.text}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function ListaContato() {
+  return (
+    <ul>
+      {contato.map((item, index) => (
+        <><li key={index} className="listas1">
+          {item.text}
+        </li>
+        <li className="listas1">
+            {item.num}
+          </li></>
+      ))}
+    </ul>
+  );
+}
+
+function ListaCategorias() {
+  return (
+    <ul>
+      {categorias.map((item, index) => (
+        <li key={index} className="listass">
+          <NavLink to={item.link} className="links">
+            {item.text}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 const Footer = () => {
   return (
@@ -52,23 +180,40 @@ const Footer = () => {
             <figure>
               <img src={Logob} alt="Logo da loja" />
             </figure>
-            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit, Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
             <div className="redes">
               <a href="#"><img src={facebook} alt="Facebook da loja" /></a>
               <a href="#"><img src={instagram} alt="Instagram da loja" /></a>
               <a href="#"><img src={twitter} alt="Twitter da loja" /></a>
             </div>
           </div>
+          <div className="tudo">
           <div className="informação">
-            
+            <div className="titulo">
+              <h3>Informação</h3>
+            </div>
+            <div className="topico">
+              <ListaInformacoes/>
+            </div>
           </div>
           <div className="categorias">
-            
+            <div className="titulo">
+              <h3>Categorias</h3>
+            </div>
+            <div className="topico">
+              <ListaCategorias/>
+            </div>
           </div>
           <div className="contato">
-            
+            <div className="titulo">
+              <h3>Contato</h3>
+            </div>
+            <div className="topico">
+              <ListaContato/>
+            </div>
           </div>
         </div>
+          </div>
         <hr/>
         <div className="ultimo">
           <h5>@ 2026 Digital Store</h5>
