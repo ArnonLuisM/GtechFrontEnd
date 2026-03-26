c67388d5c156ea01e9bec3ee7a25328d74433f05
Arnon <arnonluism@gmail.com>
2026-03-26 18:14:07 -0300

    outra tentativa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Logo from './logo';
+import Logo from './Logo';
 import SearchBar from './searchBar';
 import AuthLinks from './authLinks';
 import MainNav from './mainNave';
